f9da3337838c3c1c1a23c9b2c07fef077c5660f6
Kishan Kumar Das <kishankumar2672001@gmail.com>
2024-12-06 03:48:37 -0500

    project added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Example: Form submission alert
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you for contacting us!");
+  });
+  
